e38c1544aeda715dbf216c3d7757a2e96c4fab1b
Braden Soller <braden.soller18@gmail.com>
2024-01-29 15:41:30 -0600

    created "delete" route in admin.router.js

--- server/routes/admin.router.js
+++ server/routes/admin.router.js
@@ -2,7 +2,23 @@ const express = require("express");
 const pool = require("../modules/pool");
 const router = express.Router();
 
-
+router.delete('/:id', (req, res) => {
+    const query = `
+      DELETE FROM "posts"
+      WHERE "id" = $1;
+    `;
+    const values = [req.params.id];
+  
+    pool
+      .query(query, values)
+      .then(result => {
+        res.sendStatus(201);
+      })
+      .catch((err) => {
+        console.error('DELETE route failed:', err);
+        res.sendStatus(500);
+      });
+  });
 
 
 
