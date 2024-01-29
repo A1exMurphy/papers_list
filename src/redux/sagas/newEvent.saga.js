522df89074db5743e4a947ecdd1af84701469eca
Joshua Duenow <joshjduenow@gmail.com>
2024-01-29 15:36:11 -0600

    created new event saga

---
+++ src/redux/sagas/newEvent.saga.js
@@ -0,0 +1,17 @@
+import { put, takeLatest } from "redux-saga/effects";
+import axios from "axios";
+
+function* addNewEvent(action) {
+  try {
+    const response = yield axios({
+      method: "POST",
+      url: "/api/newevent",
+      data: action.payload
+    });
+  } catch (error) {
+    console.log("Unable to post new events to server", error);
+  }
+}
+export default function* newEventSaga() {
+  yield takeLatest("ADD_EVENT", addNewEvent);
+}
