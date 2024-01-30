535443cc0486df7afbe5a3ac967092ca1261988c
Evan Langer <elanger@live.com>
2024-01-30 16:42:11 -0600

    complete post & delete routes in tag.router, incomplete GET

--- server/routes/tags.router.js
+++ server/routes/tags.router.js
@@ -14,19 +14,19 @@ router.get('/:id', (req, res) => {
             ON "posts"."id"="post_tags"."post_id"
         LEFT JOIN "tags"
             ON "post_tags"."tag_id"="tags"."id"
-        WHERE "id" = $1
+        WHERE "posts"."id" = $1;
     `
     postID = [req.params.id]
    
   
     pool.query(getSelectedEvent, postID)
       .then((result) => {
-        res.send(result.rows[0])
+        res.send(result.rows)
         
   
       })
       .catch((err) => {
-        console.log("GET /api/eventfeed fail:", err);
+        console.log("error in GET query", err);
         res.sendStatus(500);
       });
 });
@@ -34,42 +34,30 @@ router.get('/:id', (req, res) => {
 /**
  * PUT route template
  */
-//submit PUT request for selected event with new or existing values
-router.put('/:id', (req, res) => {
-    console.log('in PUT query')
+//submit POST request for a new tag
+router.post('/', (req, res) => {
+    console.log('in POST query')
 
-    const insertNewEvent = 
+    const insertNewTag = 
     `
-    UPDATE "posts" 
-	    SET
-            "host"=$1,
-            "event_name"=$2,
-            "cost"=$3,
-            "time"=$4,
-            "description"=$5,
-            "event_size"=$6,
-            "image"=$7
-        
-    WHERE "id" = $8;
+    INSERT INTO "tags" (
+        "tag_name"
+    )
+	    VALUES (
+            $1
+        );
     `
-//not sure if the happy path is using req.body.id or req.params.id
-    newEventValues = [
-        req.body.host,
-        req.body.event_name,
-        req.body.cost,
-        req.body.time,
-        req.body.description,
-        req.body.event_size,
-        req.body.image,
-        req.body.id
+
+    newTagValues = [
+        req.body.tag_name
     ]
 
-        pool.query(insertNewEvent, newEventValues)
+        pool.query(insertNewTag, newTagValues)
             .then((result) => {
                 res.sendStatus(201)
             })
             .catch((err) => {
-                console.log(err, 'error in PUT query')
+                console.log(err, 'error in POST query')
                 res.sendStatus(500)
             })
 });
@@ -78,13 +66,13 @@ router.put('/:id', (req, res) => {
 //initiate DELETE query for selected id
 router.delete('/:id', (req, res) => {
     const query = `
-      DELETE FROM "posts"
+      DELETE FROM "tags"
       WHERE "id" = $1;
     `;
-    const values = [req.params.id];
+    const tagID = [req.params.id];
   
     pool
-      .query(query, values)
+      .query(query, tagID)
       .then(result => {
         res.sendStatus(201);
       })
