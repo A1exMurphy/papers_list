5a7c3ca03f6ca09a1d2f2e23305ee616feeab60d
Joshua Duenow <joshjduenow@gmail.com>
2024-01-30 09:44:45 -0600

    created contactInfo saga

---
+++ src/redux/sagas/contactInfo.saga.js
@@ -0,0 +1,18 @@
+import { put, take, takeLatest } from "redux-saga/effects";
+import axios from "axios";
+
+function* submitContactInfo(action) {
+    try {
+        const response = yield axios ({
+            method: "POST",
+            url: "/api/contact",
+            data: action.payload,
+        });
+    } catch (error) {
+        console.log("Unable to post contact info to server", error);
+    }
+
+}
+export default function* contactSaga() {
+    yield takeLatest("ADD_CONTACT", submitContactInfo)
+}
