f912d2ceb4e5fd46c3ad139b4d83324eabd51497
Joshua Duenow <joshjduenow@gmail.com>
2024-01-31 15:14:54 -0600

    more nav bar changes and linking to home page

--- src/components/AdminArchive/AdminArchive.jsx
+++ src/components/AdminArchive/AdminArchive.jsx
@@ -1,87 +1,74 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
-import './AdminArchive.css';
+import "./AdminArchive.css";
 
-export default function AdminArchive({  }) {
+export default function AdminArchive({}) {
+  useEffect(() => {
+    dispatch({ type: "FETCH_ARCHIVED_EVENTS" });
+    dispatch({ type: "FETCH_TAGS" });
+    window.scrollTo(0, 0);
+  }, []);
 
-    
+  let [tagId, setTagId] = useState(0);
 
-    useEffect(() => {
-        dispatch({ type: "FETCH_ARCHIVED_EVENTS" })
-        dispatch({ type: "FETCH_TAGS" })
-        window.scrollTo(0, 0)
-    }, [])
+  const events = useSelector((store) => store.archived);
+  const tags = useSelector((store) => store.tags);
+  console.log("events", events);
 
-    let [tagId, setTagId] = useState(0)
+  const history = useHistory();
+  const dispatch = useDispatch();
 
-    const events = useSelector(store => store.archived)
-    const tags = useSelector(store => store.tags)
-    console.log('events', events);
-    
+  const deleteTag = (tag) => {
+    dispatch({
+      type: "DELETE_FROM_ARCHIVE",
+      payload: tag,
+    });
+  };
 
-    const history = useHistory()
-    const dispatch = useDispatch()
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Host</th>
+            <th>Event Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {events.map((event) => {
+            return (
+              <tr key={event.id}>
+                <td>{event.host}</td>
+                <td>{event.event_name}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
 
-    const deleteTag = (tag) => {
-        dispatch({
-            type: "DELETE_FROM_ARCHIVE",
-            payload: tag
-
-        })
-    }
-
-        return (
-            <div>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Host</th>
-                            <th>Event Name</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {events.map((event) => {
-                            return (
-                          
-                                <tr key={event.id}>
-                                    <td >{event.host}</td>
-                                    <td>{event.event_name}</td>
-
-                                </tr>
-                            
-                            )
-
-
-                        })}
-                    </tbody>
-                </table>
-
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Tag Name</th>
-                            <th>Delete/Edit</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {tags.map((tag) => {
-                            return (
-
-                                <tr key={tag.id}>
-                                    <td >{tag.tag_name}</td>
-                                    <td><button onClick={() => deleteTag(tag)}>delete</button> <button>edit</button></td>
-                                
-
-                                </tr>
-
-                            )
-
-
-                        })}
-                    </tbody>
-                </table>
-                <button>Add Tag</button>
-            </div>
-        )
-    }
+      <table>
+        <thead>
+          <tr>
+            <th>Tag Name</th>
+            <th>Delete/Edit</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tags.map((tag) => {
+            return (
+              <tr key={tag.id}>
+                <td>{tag.tag_name}</td>
+                <td>
+                  <button onClick={() => deleteTag(tag)}>delete</button>{" "}
+                  <button>edit</button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <button>Add Tag</button>
+    </div>
+  );
+}
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,15 @@
 import "./Header.css";
+import { Link } from "react-router-dom";
+
+// Paper's List header is a clickable link to /home
 
 export default function Header() {
   return (
-    <h2 className="nav-title">
-      Paper'
-      <div className="s">S</div> List
-    </h2>
+    <Link to="/home">
+      <h2 className="nav-title">
+        Paper'
+        <div className="s">S</div> List
+      </h2>
+    </Link>
   );
 }
