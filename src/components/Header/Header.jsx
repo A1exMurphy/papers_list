0a10e898fb447ae68c1e4f6a962f4d974be5bb2c
Joshua Duenow <joshjduenow@gmail.com>
2024-01-31 14:17:29 -0600

    created header component and separated it from the nav component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,8 @@
 import "./Header.css";
 
 export default function Header() {
-return <span><h2 className="nav-title">Paper'<div className="s">S</div> List</h2></span>
-
+return (<span>
+    <h2 className="nav-title">Paper'
+    <div className="s">S</div> List</h2></span>
+)
 }
