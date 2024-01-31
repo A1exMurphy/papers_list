95e51a6f84d8796baa345be34e16e7238d18c5e6
Joshua Duenow <joshjduenow@gmail.com>
2024-01-31 14:41:10 -0600

    more header changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,10 @@
 import "./Header.css";
 
 export default function Header() {
-return (<span>
-    <h2 className="nav-title">Paper'
-    <div className="s">S</div> List</h2></span>
-)
+  return (
+    <h2 className="nav-title">
+      Paper'
+      <div className="s">S</div> List
+    </h2>
+  );
 }
