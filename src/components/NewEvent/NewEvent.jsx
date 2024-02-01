007f19640497779b02cd25721389ba055afb6d4f
Christian Carlson <christiancarlson648@gmail.com>
2024-02-01 10:25:13 -0600

    Finished functionality for a lot of things

--- src/components/NewEvent/NewEvent.jsx
+++ src/components/NewEvent/NewEvent.jsx
@@ -1,6 +1,7 @@
 
-import { useState } from 'react';
+import { useState} from 'react';
 import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import './NewEvent.css';
 
 export default function NewEvent() {
@@ -8,27 +9,41 @@ export default function NewEvent() {
     let [hostInput, setHostInput] = useState('');
     let [locationInput, setLocationInput] = useState('');
     let [dateInput, setDateInput] = useState('');
+    let [descriptionInput, setDescriptionInput] = useState('');
+    let [image, setImage] = useState('');
+    let [eventSize, setEventSize] = useState('');
     let [costInput, setCostInput] = useState('');
+  
     const dispatch = useDispatch();
+    const history = useHistory();
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
+        const newEvent = {
+            event_name: titleInput, 
+            host: hostInput,
+            time: dateInput,
+            cost: costInput,
+            location: locationInput,
+            description: descriptionInput,
+            image: image,
+            event_size: eventSize
+        }
+
+        console.log(newEvent);
+
         dispatch({
             type: 'ADD_EVENT',
-            payload: {
-                title: titleInput, 
-                host: hostInput,
-                date: dateInput,
-                cost: costInput,
-                location: locationInput
-            }
+            payload: newEvent
         })
         console.log("Handling submit");
     }
 
-    const handleDiscard = () => {
+    const handleDiscard = (e) => {
         console.log("Handling discard");
+
+        history.push("/");
     }
     return (
         <>
@@ -76,6 +91,33 @@ export default function NewEvent() {
                         type='text'
                         onChange={e => setCostInput(e.target.value)}
                         value={costInput}
+                    />
+                </div>
+                <div>
+                    <label id='description-label' htmlFor='description-input'>Description</label>
+                    <input
+                        id='description-input'
+                        type='text'
+                        onChange={e => setDescriptionInput(e.target.value)}
+                        value={descriptionInput}
+                    />
+                </div>
+                <div>
+                    <label id='image-label' htmlFor='image-input'>Image</label>
+                    <input
+                        id='image-input'
+                        type='text'
+                        onChange={e => setImage(e.target.value)}
+                        value={image}
+                    />
+                </div>
+                <div>
+                    <label id='event-size-label' htmlFor='size-input'>Event Size</label>
+                    <input
+                        id='size-input'
+                        type='text'
+                        onChange={e => setEventSize(e.target.value)}
+                        value={eventSize}
                     />
                 </div>
                 <button className='submit-btn'>Submit</button>
