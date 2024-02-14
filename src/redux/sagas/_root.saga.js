469578182608295f38604a1d9ca1c88c3f347ab6
Joshua Duenow <joshjduenow@gmail.com>
2024-02-14 10:46:31 -0600

    rename event saga file

--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -2,7 +2,7 @@ import { all } from 'redux-saga/effects';
 import loginSaga from './login.saga';
 import registrationSaga from './registration.saga';
 import userSaga from './user.saga';
-import eventSaga from './landingPage.saga';
+import eventSaga from './event.saga';
 import archivedEventSaga from './adminArchive.saga';
 import newEventSaga from './newEvent.saga';
 import contactSaga from './contactInfo.saga';
