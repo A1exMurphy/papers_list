bfc5425aa1a968dd1d37c9db8abacabcf49daaa7
Evan Langer <elanger@live.com>
2024-02-29 15:51:17 -0600

    made css for DisplayContact

--- src/components/ContactInfo/DisplayContact.jsx
+++ src/components/ContactInfo/DisplayContact.jsx
@@ -1,13 +1,12 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory, useParams } from "react-router-dom/cjs/react-router-dom.min";
+import "./DisplayContact.css";
 
 export default function DisplayContact(eventContact)  {
     const dispatch = useDispatch();
     const history = useHistory();
     const params = useParams();
-    
-    
     
     useEffect(() => {
         dispatch({
@@ -17,14 +16,33 @@ export default function DisplayContact(eventContact)  {
     }, []);
     
     const contact = useSelector((store) => store.selectedContact);
-    console.log(contact, "Display contact's info")
+    // console.log(contact, "Display contact's info")
 
     return (
-        <div>
-        <h1>Contact Info</h1>
-        <h3>{contact[0]?.email}</h3>
-        <h3>{contact[0]?.phone}</h3>
-
+        <div className="contact-post-it">
+        <h1 className="contact-header">Contact Info</h1>
+            <div className="grid-container">
+                
+                    <div className="grid-item1">EMAIL</div>
+                    <div className="grid-item2">:</div>
+                    <div className="grid-item3"> {contact[0]?.email}</div>
+                
+                
+                    <div className="grid-item1">PHONE</div>
+                    <div className="grid-item2">:</div>
+                    <div className="grid-item3"> {contact[0]?.phone}</div>
+                
+                
+                    <div className="grid-item1">LINKEDIN</div>
+                    <div className="grid-item2">:</div>
+                    <div className="grid-item"> {contact[0]?.linkedIn}</div>
+                
+                
+                    <div className="grid-item1">INFO</div>
+                    <div className="grid-item2">:</div>
+                    <div className="grid-item3"> {contact[0]?.additional_info}</div>
+                
+            </div>
         </div>
     )
 }
